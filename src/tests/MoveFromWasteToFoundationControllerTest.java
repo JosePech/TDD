0fe37bd42f4dd0751e0b8568ca56dcb2f57cede2
JosePech <jrocks169@gmail.com>
2015-04-07 18:12:49 +0200

    Caso de prueba: moveFromWasteToFullFoundation

--- src/tests/MoveFromWasteToFoundationControllerTest.java
+++ src/tests/MoveFromWasteToFoundationControllerTest.java
@@ -40,6 +40,25 @@ public class MoveFromWasteToFoundationControllerTest {
         assertEquals(1, moveController.getFoundation(2).getSize());
         assertEquals(0, moveController.getFoundation(3).getSize());
     }
+    
+    @Test
+    public void moveFromWasteToFullFoundationTest() {
+        moveController.moveToFoundation(new Card(Suit.A, 1), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 2), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 3), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 4), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 5), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 6), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 7), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 8), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 9), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 10), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 11), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 12), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 13), 0);
+        moveController.moveToFoundation(new Card(Suit.A, 14), 0);
+        assertEquals(13, moveController.getFoundation(0).getSize());
+    }
 
     private Board createEmptyFoundation() {
         Board board = new Board();
